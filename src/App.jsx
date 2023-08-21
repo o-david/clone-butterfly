982e1c0945c5f21fa1493f2ba80c991853009c00
David <okekedavid1333@gmail.com>
2023-08-21 14:00:28 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Home from './components/Home'
+import Countries from './components/Countries'
+import Footer from './components/Footer'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Home />
+      <Countries />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>this is a second trioal    </div>
+  )
+}
+
+export default Footer
