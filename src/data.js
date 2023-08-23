c911e1bd86e480670785cdb6d143646435a33a13
David <okekedavid1333@gmail.com>
2023-08-23 09:29:28 +0100

    Complete the Countries component and import it

--- src/data.js
+++ src/data.js
@@ -1,91 +1,89 @@
-const data = {
-  countryDetails: [
+const data = [
     {
       id: 1,
       img: "https://www.butterfly.tt/flags/albania.svg",
-      country: "Albenia",
+      name: "Albenia",
       europe: true,
       storeLink: "https://butterfly-adria.com/",
     },
     {
       id: 2,
       img: "https://www.butterfly.tt/flags/austria.svg",
-      country: "Austria",
+      name: "Austria",
       europe: true,
       storeLink: "https://butterfly-adria.com/",
     },
     {
       id: 3,
       img: "https://www.butterfly.tt/flags/belarus.svg",
-      country: "Belarus",
+      name: "Belarus",
       europe: true,
       storeLink: "https://butterfly-adria.com/",
     },
     {
       id: 4,
       img: "https://www.butterfly.tt/flags/belgium.svg",
-      country: "Belgium",
+      name: "Belgium",
       europe: true,
       storeLink: "https://butterfly-adria.com/",
     },
     {
       id: 5,
       img: "https://www.butterfly.tt/flags/bosnia-and-herzegovina.svg",
-      country: "Bosnia and Herzegovina",
+      name: "Bosnia and Herzegovina",
       europe: true,
       storeLink: "https://butterfly-adria.com/",
     },
     {
       id: 6,
       img: "https://www.butterfly.tt/flags/bulgaria.svg",
-      country: "Bulgaria",
+      name: "Bulgaria",
       europe: true,
       storeLink: "https://butterfly-adria.com/",
     },
     {
       id: 7,
       img: "https://www.butterfly.tt/flags/australia.svg",
-      country: "Australia",
+      name: "Australia",
       europe: false,
       storeLink: "https://butterfly-adria.com/",
     },
     {
       id: 8,
       img: "https://www.butterfly.tt/flags/brazil.svg",
-      country: "Brazil",
+      name: "Brazil",
       europe: false,
       storeLink: "https://butterfly-adria.com/",
     },
     {
       id: 9,
       img: "https://www.butterfly.tt/flags/canada.svg",
-      country: "Canada",
+      name: "Canada",
       europe: false,
       storeLink: "https://butterfly-adria.com/",
     },
     {
       id: 10,
       img: "https://www.butterfly.tt/flags/china.svg",
-      country: "China",
+      name: "China",
       europe: false,
       storeLink: "https://butterfly-adria.com/",
     },
     {
       id: 11,
       img: "https://www.butterfly.tt/flags/egypt.svg",
-      country: "Egypt",
+      name: "Egypt",
       europe: false,
       storeLink: "https://butterfly-adria.com/",
     },
     {
       id: 12,
       img: "https://www.butterfly.tt/flags/iran.svg",
-      country: "Iran",
+      name: "Iran",
       europe: false,
       storeLink: "https://butterfly-adria.com/",
     },
         
-  ],
-};
+  ]
 
 export default data;
