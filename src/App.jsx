9d2937a3a8c80c7f24a46d088f417291ab390271
David <okekedavid1333@gmail.com>
2024-01-04 18:51:13 +0100

    adding extra links 3

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
       <BrowserRouter>
           <Routes>
             <Route path='/' element={<Home/>}/>
-            <Route path='/error' element={<Error/>}/>
+            <Route path='/*' element={<Error/>}/>
           </Routes>
         </BrowserRouter>
     </>
